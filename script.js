776df9aab4a824f3d7e0a602c8524a7c34e14149
avongalie <alexsiaavila@gmail.com>
2022-03-09 10:05:58 -0500

    added notes

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 const play = document.getElementById("play");
 const gameboard = document.getElementById("gameboard");
 
-console.log(play.children[0])
-console.log(gameboard)
+// console.log(play.children[0])
+// console.log(gameboard)
 
 let currentchoice = 0;
 let currentcolor = 'red';
 
-play.onclick = function(event){
-    play.children[currentchoice].innerHTML = "";
-    let targete = event.target
-    console.log(targete.id)
-    let x = document.createElement("img");
-    x.src ="images/red_puck.png";
-    x.style.width = "93px";
-    currentchoice = targete.id-1;
-    targete.appendChild(x);
-}
+// play.onclick = function(event){
+//     play.children[currentchoice].innerHTML = "";
+//     let targete = event.target
+//     console.log(targete.id)
+//     let x = document.createElement("img");
+//     x.src ="images/red_puck.png";
+//     x.style.width = "93px";
+//     currentchoice = targete.id-1;
+//     targete.appendChild(x);
+// }
 
 play.children[0].appendChild(puckimg('red'))
 
+//controls keyboard listeners
 window.addEventListener('keydown', function(event){
     console.log(event);
     let p = puckimg(currentcolor);
@@ -41,6 +42,7 @@ window.addEventListener('keydown', function(event){
     }
 })
 
+//switchs color of puck
 function toggleColor(){
     if(currentcolor === 'red'){
         currentcolor = 'yellow'
@@ -48,13 +50,15 @@ function toggleColor(){
         currentcolor = 'red'
     }
 }
-console.log(gameboard.children[21]);
+
 0
 7
 14
 21
 28
 35
+
+//creates new puck image
 function puckimg(color){
     let x = document.createElement("img");
     x.style.width = "93px";
@@ -69,6 +73,7 @@ function puckimg(color){
     }
 }
 
+//places puck in correct space
 function playerTurn(x, color){
     if(gameboard.children[x+35].innerHTML === ""){
         gameboard.children[x+35].appendChild(puckimg(color));
@@ -111,9 +116,16 @@ function playerTurn(x, color){
         play.children[currentchoice].innerHTML = "";
         currentchoice = 0;
         play.children[currentchoice].appendChild(puckimg(currentcolor))
-
+        
     }else{
         return;
     }
+    console.log(gameboard.children[x+35].innerHTML);
 }
 
+// let currentboard = [];
+// for(let i = 0; i < gameboard.children.length; i++){
+//     if(gameboard.children[i].innerHTML = ""){
+//         currentboard.push(0);
+//     }else if()
+// }
